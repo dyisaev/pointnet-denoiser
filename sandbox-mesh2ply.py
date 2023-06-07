cf688d55838b6ee54754e4e66b37176c48118e05
Dmitry Isaev <dmitry.isaev@duke.edu>
2023-06-07 19:07:12 -0400

    visualization with open3d and pptk

--- sandbox-mesh2ply.py
+++ sandbox-mesh2ply.py
@@ -2,10 +2,10 @@ import trimesh
 import numpy as np
 
 # Load the mesh from .off file
-mesh = trimesh.load_mesh('/Users/dmitry/projects/3D-projects/data/ModelNet10/bathtub/train/bathtub_0001.off')
+mesh = trimesh.load_mesh('/diskB/data/ModelNet10_aligned/bathtub/train/bathtub_0003.off')
 
 # Sample points uniformly from the surface of the mesh
 point_cloud = trimesh.sample.sample_surface(mesh, count=2048) # 5000 points
 
 # Save the point cloud to a .ply file
-trimesh.points.PointCloud(point_cloud[0]).export('./sandbox-data/bathtub_0001_256.ply')
+trimesh.points.PointCloud(point_cloud[0]).export('./sandbox-data/bathtub_0003_256.ply')
